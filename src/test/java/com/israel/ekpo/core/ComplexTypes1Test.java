60bb8c8857c6c20142a344ad061a0147c247bcc3
Israel Ekpo <israel.ekpo@disney.com>
2014-01-03 11:11:36 -0500

    Added complex type test

---
+++ src/test/java/com/israel/ekpo/core/ComplexTypes1Test.java
@@ -0,0 +1,34 @@
+package com.israel.ekpo.core;
+
+import org.junit.Test;
+
+/**
+ * Comparison of Objects
+ *
+ * @author iekpo
+ *
+ */
+public class ComplexTypes1Test {
+
+    @Test
+    public void compareStudents1() {
+
+        // In this test we are going to create 2 students with the same property values and compare them
+    }
+
+    @Test
+    public void compareStudents2() {
+
+        // In this test we are going to create 2 students with only one different property and compare them
+    }
+
+    @Test
+    public void compareEmployees1() {
+
+    }
+
+    @Test
+    public void compareHouseWifes1() {
+
+    }
+}
